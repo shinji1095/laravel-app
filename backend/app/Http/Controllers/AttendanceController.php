08886ce7ef30e2a20a596e16f653a2d717bfc06a
shinji1095 <shinji1095nameko@gmail.com>
2021-05-26 21:08:24 +0900

    add AttendanceController and create route

--- backend/app/Http/Controllers/AttendanceController.php
+++ backend/app/Http/Controllers/AttendanceController.php
@@ -6,5 +6,18 @@ use Illuminate\Http\Request;
 
 class AttendanceController extends Controller
 {
-    //
+    public function attend(Request $request){
+        return "hello"
+    }
+
+    public function leave(Request $request){
+        $leave  = $request["leave"];
+        $lunch  = $requset["lunch"];
+        $dinner = $request["dinner"];
+        return $request
+    }
+
+    public function temp(Request $request){
+        return 
+    }
 }
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -15,4 +15,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+
+Route::get("temp", "AttendanceController@temp")
+
+# Ajax通信
+Route::prefix("ajax")->group(function(){
+    Route::post("attend", "AttendanceController@attend")
+    Route::post("leave", "AttendanceController@leave")
+
+})
 });
